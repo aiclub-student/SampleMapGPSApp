8fdec97044a5b47c5174e480a1df4d5ccf0e9524
Danika Gupta <danika@gprof.com>
2023-08-25 21:38:45 -0700

    Initial Commit

---
+++ SampleMapGPSApp/SampleMapGPSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMapGPSAppApp.swift
+//  SampleMapGPSApp
+//
+//  Created by Amit Gupta on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleMapGPSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
